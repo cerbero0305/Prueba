f1322d2cf77059185eac6c78f3f6ad65e94af376
cerbero0305 <zon03ab@gmail.com>
2025-11-03 11:30:45 -0500

    Update config example.php

--- config/config example.php
+++ config/config example.php
@@ -1,7 +1,7 @@
 <?php
 const BD_HOST = 'localhost';
-const BD_NAME = 'inventario'; 
-const BD_USER = 'root';
+const BD_NAME = ''; 
+const BD_USER = '';
 const BD_PASSWORD = '';
 const BD_CHARSET = 'utf8';
 
